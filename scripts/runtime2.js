4556f0b298672d729ecd2b736b62d1823f5b6d97
khpeet <kpeet@newrelic.com>
2023-04-21 12:53:14 -0400

    kobe12

--- scripts/runtime2.js
+++ scripts/runtime2.js
@@ -1,1 +1,1 @@
-dfkmsdfsasddf
+dfkmsdfsasddfasda
--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-changed_files=$(echo "[\"scripts/New Runtime Example.js\"]" | jq -r '.[]')
+# file="ABC I&O: https://cooperost.icsconnect.com/.js"
+#
+# filename=$(basename $file)
+# filename=${filename%.*}
+# echo $filename
+# jsname=${filename//[ &:/\\\.]/_}
+#
+# echo $jsname
+
+
+# string="ABC I&O: https://cooperost.icsconnect.com/.js"
+string="new_runteimt_a-fun.js"
+
+# remove any leading or trailing spaces
+string=$(echo "$string" | xargs)
+
+# extract characters before .js
+filename=${string%%.js}
+
+echo $filename
