314cf5f6a4fec949b4468ee627429bbae57737b0
khpeet <kpeet@newrelic.com>
2023-04-21 15:53:17 -0400

    kobe14

--- scripts/runtime2.js
+++ scripts/runtime2.js
@@ -1,3 +1,3 @@
 dfkmsdfsasddfasda
 safafs
-asd
+asdasds
--- test.sh
+++ test.sh
@@ -10,13 +10,25 @@
 # echo $jsname
 
 
-# echo "[\"scripts/New Runtime Example.js\",\"scripts/runtime2.js\"]" | jq -r '.[]'
-changed_files=$(echo "[\"scripts/New Runtime Example.js\",\"scripts/runtime2.js\"]" | jq -r '.[]')
-echo $changed_files
-filenames=()
-for file in $changed_files; do
+# FILES=/Users/kpeet/newrelic-synthetics-sync/scripts/*
+#
+# for f in $FILES; do
+#   echo $f
+# done
+string="scripts/ABC I&O: https://cooperost.icsconnect.com/.js scripts/runtime2.js"
+
+# split string into an array
+IFS=' ' read -ra files <<< "$string"
+
+# extract only file names and add to array
+js_files=()
+for file in "${files[@]}"; do
+  filename=$(basename "$file")
   monitorName=$(echo basename ${f%.*} | sed "s/.*\///")
   echo $monitorName
-  filenames+=("$monitorName")
+  # filename=$(echo "$filename" | xargs)
+  # jsname=${filename%%.js}
+  monitors+=("$jsname")
 done
-echo $filenames
+
+echo "${monitors[@]}"
