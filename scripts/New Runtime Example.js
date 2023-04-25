0d2e6e60ad01430dc6a93d91ebd4d10a8437adc8
khpeet <kpeet@newrelic.com>
2023-04-25 10:11:27 -0400

    kobe24

--- scripts/New Runtime Example.js
+++ scripts/New Runtime Example.js
@@ -4,7 +4,7 @@
 var elementWaitTimer = 10000;
 
 const url_to_monitor = 'https://www.tutorialrepublic.com/snippets/preview.php?topic=bootstrap&file=simple-login-form';
-const user = 'keagan333ssss';
+const user = 'keagan333sssss';
 const pass = 'test222sss';
 /** CONFIGURATION **/
 
